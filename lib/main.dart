3a479940213d7f1456f6f90eccb566b934d3902d
FlutLab.io <>
2022-04-20 15:12:36 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Application name
+      title: 'Flutter Hello World',
+      // Application theme data, you can set the colors for the application as
+      // you want
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      // A widget which will be started on application startup
+      home: MyHomePage(title: 'Products'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // The title text which will be shown on the action bar
+          title: Text(title),
+        ),
+        body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, index) {
+              return Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: ListTile(
+                      leading: Image.network("https://images.pexels.com/photos/5586483/pexels-photo-5586483.jpeg?cs=srgb&dl=pexels-tima-miroshnichenko-5586483.jpg&fm=jpg"),
+                      title: Text(
+                        "Laptop Toshiba Core i5",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      subtitle: Text(
+                        "Lorem Ipsum dolor sit amet, cons",
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      )));
+            }));
+  }
+}
